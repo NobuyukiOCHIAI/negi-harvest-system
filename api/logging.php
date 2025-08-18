cf3e372667e15f4b316ff93e77eb7e55f187c892
NobuyukiOCHIAI <42254551+NobuyukiOCHIAI@users.noreply.github.com>
2025-08-18 19:19:05 +0900

    Add files via upload

--- api/logging.php
+++ api/logging.php
@@ -1,10 +1,18 @@
 <?php
-function log_error(string $message, string $filename = 'app.log'): void {
+function log_error(string $message, array $stage): void {
     $dir = '/home/love-media/forc_logs';
     if (!is_dir($dir)) {
         mkdir($dir, 0777, true);
     }
-    $timestamp = date('Y-m-d H:i:s');
-    error_log("[$timestamp] $message\n", 3, $dir . '/' . $filename);
+	
+	$date = date("Y-m-d H:i:s");
+ 	$dateF = date("Ymd");
+	
+	$file = $dir."/".$dateF.'_error.txt';
+	$contents .= $date."\t".$message."\t".implode($stage)."\n";
+	
+	$handling = fopen($file,  'a');
+	chmod($file, 0777);
+	fwrite($handling, $contents);
 }
 ?>
